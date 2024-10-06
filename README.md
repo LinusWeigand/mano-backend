51ffd2aa8b077468ab8a90b300ce446884aec48d
Linus Weigand <linusweigand@protonmail.com>
2024-10-06 11:29:51 +0200

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Commnands
+
+1. Start Projekt with watcher
+   bash`cargo watch -q -c -w src/ -x run`
--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -4,9 +4,10 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 
 use crate::AppState;
 
-pub async fn register(
+pub async fn pre_register(
     email: &str,
     password: &str
 ) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+
     Ok(())
 }
