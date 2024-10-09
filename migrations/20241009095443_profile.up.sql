ea47af13477c61fc33bf5a2aa91e9965c370531b
Linus Weigand <linusweigand@protonmail.com>
2024-10-09 12:10:16 +0200

    add profiles table

---
+++ migrations/20241009095443_profile.up.sql
@@ -0,0 +1,25 @@
+-- Add up migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE
+    IF NOT EXISTS profiles (
+        id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+        viewer_id UUID REFERENCES viewers(id) ON DELETE CASCADE NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        craft VARCHAR(100) NOT NULL,
+        location VARCHAR(100) NOT NULL,
+        website VARCHAR(100),
+        instagram VARCHAR(100),
+        skills VARCHAR(20)[],
+        bio TEXT NOT NULL,
+        experience SMALLINT NOT NULL CHECK (experience <= 75),
+        portfolio BYTEA[] NOT NULL,
+        portfolio_bio TEXT NOT NULL,
+
+        created_at TIMESTAMP
+        WITH
+            TIME ZONE DEFAULT NOW(),
+        updated_at TIMESTAMP
+        WITH
+            TIME ZONE DEFAULT NOW()
+    );
