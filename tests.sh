fa1d4dae6cc1118d0429d0c3aad55e3302b97df3
Linus Weigand <linusweigand@protonmail.com>
2024-10-13 16:07:31 +0200

    t

--- tests.sh
+++ tests.sh
@@ -46,19 +46,17 @@ portfolio1=$(base64 -w 0 holzrausch1.png)
 portfolio2=$(base64 -w 0 holzrausch2.png)
 
 curl -X POST http://localhost/api/profile \
--H "Content-Type: application/json" \
--d '{
-  "email": "linus@couchtec.com",
-  "name": "John Doe",
-  "craft": "Carpentry",
-  "location": "New York",
-  "website": "https://johndoe.com",
-  "instagram": "https://instagram.com/johndoe",
-  "skills": ["woodworking", "furniture making", "design"],
-  "bio": "Experienced carpenter with over 10 years in the industry.",
-  "experience": 10,
-  "portfolio_bio": "Here is my portfolio showcasing my recent work."
-}'
+-H "Content-Type: multipart/form-data" \
+-F "email=linus@couchtec.com" \
+-F "name=John Doe" \
+-F "craft=Carpentry" \
+-F "location=New York" \
+-F "website=https://johndoe.com" \
+-F "google_ratings=tests" \
+-F "instagram=https://instagram.com/johndoe" \
+-F "bio=Experienced carpenter with over 10 years in the industry." \
+-F "skills=[\"Holzmöbel\",\"Küchen\",\"Badezimmer\"]" \
+-F "portfolio_bio=Here is my portfolio showcasing my recent work."
 
 
 curl -X POST http://localhost/api/search -H "Content-Type: application/json" -d '{"craft": "Zimmerei", "location": "Nürnberg"}'
